bfcab9b95b262e73468aefcd33e899b001bdc1a1
UserIrishka <irina20054321@gmail.com>
2025-09-30 11:18:18 +0300

    Rename files

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const backButton = document.getElementById("backButton");
-
-  backButton.addEventListener("click", () => {
-    window.location.href = "home_page.html"; // переход на главную страницу
-  });
-});
