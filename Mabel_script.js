43e0d7e3798cf8a8bb3147dd9556b212adf094e4
UserIrishka <irina20054321@gmail.com>
2025-09-30 22:15:25 +0300

    added Mabel

---
+++ Mabel_script.js
@@ -0,0 +1,9 @@
+// Находим изображение и аудио
+const img = document.querySelector('img[alt="Мейбл Пайнс"]');
+const audio = document.getElementById("mabel-voice");
+
+// Добавляем обработчик клика
+img.addEventListener("click", function () {
+  audio.currentTime = 0; // Сбрасываем к началу, если уже играет
+  audio.play(); // Воспроизводим звук
+});
